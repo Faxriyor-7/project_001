dfd3348211e216c00c2f477392872e089329c7e3
Faxriyor7 <faxriyorazimboyev3@gmail.com>
2024-11-21 14:15:59 +0500

    Create

---
+++ main.py
@@ -0,0 +1,57 @@
+# Exercise 1
+# numbers = input("add the numbers ::: ").split(" ")
+# numbers = [int(i) for i in numbers]
+# print(numbers)
+#
+# l = len(numbers)
+#
+# c = 0
+#
+# n = []
+#
+# for i in numbers:
+#     c +=i
+#
+# middle = c // l
+#
+# print(middle)
+#
+# for i in numbers:
+#     if middle < i:
+#         n.append(i)
+#     else:
+#         pass
+# print(n)
+
+
+# ...
+# Exercise 2
+# number = input("Enter the number :: ").split(" ")
+# number = [int(i) for i in number]
+#
+#
+#
+# for i in number:
+#     if number[1] > number[2] > number[3] and number[3] > number[4]:
+#         print(number, sep="\n")
+
+
+# ...
+# Exercise 7
+text = input("Enter the 7 character :: ")
+
+
+b = 0
+
+if "cat" in text:
+    b += 1
+    print(b)
+# for i in a:
+#     b += i
+#     print(b)
+
+
+
+
+
+
